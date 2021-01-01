79a5f6872cd8d6ed8c8d711f9c68f076e1734499
강은빈 <kub0803@naver.com>
2021-01-01 23:20:52 +0900

    타입스크립트 스택 (제네릭) 구현

--- Stack,Queue 구현/src/Stack.ts
+++ Stack,Queue 구현/src/Stack.ts
@@ -20,28 +20,29 @@ stack.push('a');
 stack.pop().substring(0) // 'a' 
 stack.pop().substring(0)// 타입 에러 */
 
-//제네릭 
+//제네릭
 //스택 자료구조를 제네릭으로 구현
-class Stack<T> { // 제네릭을 사용하겠다는 의미로 <식별자> 식별자 : 타입
+class Stack<T> {
+  // 제네릭을 사용하겠다는 의미로 <식별자> 식별자 : 타입
 
-    private data : T[] = [];
+  private data: T[] = [];
 
-    constructor() {}
+  constructor() {}
 
-    push(item: T): void{
-        this.data.push(item);
-    }
+  push(item: T): void {
+    this.data.push(item);
+  }
 
-    pop() : T{
-        return this.data.pop();
-    }
+  pop(): T {
+    return this.data.pop();
+  }
 }
 
 //생성자를 호출하여 객체를 만들때 T 로 사용될 타입을 지정해주면 된다.
-const numberStack = new Stack<number>(); 
+const numberStack = new Stack<number>();
 const stringStack = new Stack<string>();
 
 //각 스택은 생성할 때 선언한 타입만을 저장하고 리턴
-//장점 : 컴파일러가 리턴하는 타입을 알 수 있어 에디터에서 자동 완성 사용 할 수 있어 생산성 향상에 기여한다. 
+//장점 : 컴파일러가 리턴하는 타입을 알 수 있어 에디터에서 자동 완성 사용 할 수 있어 생산성 향상에 기여한다.
 numberStack.push(1);
-stringStack.push('a');
+stringStack.push("a");
