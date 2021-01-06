2c685c11113efb3cdf54651adefd10e80c3bf5f3
강은빈 <kub0803@naver.com>
2021-01-06 23:34:34 +0900

    인터페이스 연습

---
+++ TS 연습/dist/일반객체 interface 타입.js
@@ -0,0 +1,10 @@
+"use strict";
+var person = {
+    name: "강은빈",
+    age: 18,
+};
+var expert = {
+    name: "silverbeen",
+    skills: ["react", "JS"],
+};
+var people = [person, expert];
---
+++ TS 연습/src/일반객체 interface 타입.ts
@@ -0,0 +1,22 @@
+interface Person {
+  name: string;
+  //물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값
+  age?: number;
+}
+interface Developer {
+  name: string;
+  age?: number;
+  skills: string[];
+}
+
+const person: Person = {
+  name: "강은빈",
+  age: 18,
+};
+
+const expert: Developer = {
+  name: "silverbeen",
+  skills: ["react", "JS"],
+};
+
+const people : Person[] = [person, expert];
