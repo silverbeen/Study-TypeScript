d329096ce01426eb35153721a2d947d9fbbbeb12
강은빈 <kub0803@naver.com>
2021-02-18 00:08:57 +0900

    블록체인 기능 완성

--- Typechain/dist/index.js
+++ Typechain/dist/index.js
@@ -1,11 +1,93 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var person = {
-    name: "silvebeen",
-    gender: "여자",
-    age: 18,
+var CryptoJS = __importStar(require("crypto-js"));
+var Block = /** @class */ (function () {
+    function Block(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+    Block.calculateBlockHash = function (index, previousHash, timestamp, data) {
+        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    }; // 문자열로 반환
+    //불린인데 들어온 블록의 구조가 유효한지 아닌지 판단
+    Block.validataStructor = function (aBlock) {
+        return typeof aBlock.index === "number" &&
+            typeof aBlock.hash === "string" &&
+            typeof aBlock.previousHash === "string" &&
+            typeof aBlock.timestamp === "number" &&
+            typeof aBlock.data === "string";
+    };
+    return Block;
+}());
+//static 으로 선언하지 않았더라면 사용 못함
+//Block.calculateBlockHash();
+var genesisBlock = new Block(0, "sdfsdfsdfsddsf", "", "hello", 1234567);
+var blockchain = [genesisBlock];
+var getBlockchain = function () { return blockchain; };
+var getLatestBlock = function () { return blockchain[blockchain.length - 1]; };
+var getNewTimeStamp = function () { return Math.round(new Date().getTime() / 1000); };
+//새로운 블럭을 생성하는 함수
+var createNewBlock = function (data) {
+    var previousBlock = getLatestBlock(); //새로운 인덱스를 가져옴
+    var newIndex = previousBlock.index + 1; //
+    var newTimestamp = getNewTimeStamp();
+    var newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    var newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
 };
-var sayHi = function (person) {
-    return "Hello " + person.name + " I am age " + person.age + " My gender is " + person.gender + " ";
+//console.log(createNewBlock("hello"), createNewBlock("잘가"));
+var getHashforBlock = function (aBlock) {
+    return Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 };
-console.log(sayHi(person));
+//candidataBlock, previousBlock 를 받고 유효하지 않으면 false
+var isBlockVaild = function (candidataBlock, previousBlock) {
+    //첫번째는 블록의 구조가 유효한지?
+    if (!Block.validataStructor(candidataBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidataBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidataBlock.previousHash) {
+        return false;
+    }
+    // 블록은 candidataBlock 블록이고, 얻은 해쉬가 candidata 블록의 해쉬와 같지 않으면
+    else if (getHashforBlock(candidataBlock) !== candidataBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+var addBlock = function (candidataBlock) {
+    if (isBlockVaild(candidataBlock, getLatestBlock())) {
+        blockchain.push(candidataBlock);
+    }
+};
+createNewBlock("second Block");
+createNewBlock("3번쨰 Block");
+createNewBlock("4번째 Block");
+console.log(blockchain);
--- Typechain/src/index.ts
+++ Typechain/src/index.ts
@@ -1,34 +1,126 @@
-/* interface Human {
-  name: string;
-  gender: string;
-  age: number;
-} */
+import * as CryptoJS from "crypto-js";
+import { TypeFormatFlags } from "typescript";
 
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
+class Block {
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString(); // 문자열로 반환
 
-  // 클래스로부터 객체를 만들때마다 생성 됨
-  constructor(name: string, age: number, gender: string) {
-    this.name = name; // 같은 속성의 이름은 생성자의 이름과 같다고 표기
-    this.age = age;
-    this.gender = gender;
+  //불린인데 들어온 블록의 구조가 유효한지 아닌지 판단
+  static validataStructor = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-/* const person = {
-  name: "silvebeen",
-  gender: "여자",
-  age: 18,
-}; */
+//static 으로 선언하지 않았더라면 사용 못함
+//Block.calculateBlockHash();
 
-const silver = new Human("silver", 18, "여자");
+const genesisBlock: Block = new Block(
+  0,
+  "sdfsdfsdfsddsf",
+  "",
+  "hello",
+  1234567
+);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name} I am age ${person.age} My gender is ${person.gender} `;
+let blockchain: [Block] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain; // 블럭이 아닌 것들은 암함 
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+//새로운 블럭을 생성하는 함수
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock(); //새로운 인덱스를 가져옴
+  const newIndex: number = previousBlock.index + 1; //
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  addBlock(newBlock);
+
+  return newBlock;
 };
 
-console.log(sayHi(silver));
+//console.log(createNewBlock("hello"), createNewBlock("잘가"));
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+//candidataBlock, previousBlock 를 받고 유효하지 않으면 false
+const isBlockVaild = (candidataBlock: Block, previousBlock: Block): boolean => {
+  //첫번째는 블록의 구조가 유효한지?
+  if (!Block.validataStructor(candidataBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidataBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidataBlock.previousHash) {
+    return false;
+  }
+  // 블록은 candidataBlock 블록이고, 얻은 해쉬가 candidata 블록의 해쉬와 같지 않으면
+  else if (getHashforBlock(candidataBlock) !== candidataBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidataBlock: Block): void => {
+  if (isBlockVaild(candidataBlock, getLatestBlock())) {
+    blockchain.push(candidataBlock);
+  }
+};
+
+createNewBlock("second Block");
+createNewBlock("3번쨰 Block");
+createNewBlock("4번째 Block");
+
+console.log(blockchain);
 
 export {};
