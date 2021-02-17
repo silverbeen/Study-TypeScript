ab6e790c467fc381fdb8157e9c1e4b213f4af71c
강은빈 <kub0803@naver.com>
2021-02-17 23:05:59 +0900

    Ts class 예제

--- Typechain/src/index.ts
+++ Typechain/src/index.ts
@@ -1,19 +1,34 @@
-interface Human {
+/* interface Human {
   name: string;
   gender: string;
   age: number;
+} */
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  // 클래스로부터 객체를 만들때마다 생성 됨
+  constructor(name: string, age: number, gender: string) {
+    this.name = name; // 같은 속성의 이름은 생성자의 이름과 같다고 표기
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
+/* const person = {
   name: "silvebeen",
   gender: "여자",
   age: 18,
-};
+}; */
+
+const silver = new Human("silver", 18, "여자");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name} I am age ${person.age} My gender is ${person.gender} `;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(silver));
 
 export {};
