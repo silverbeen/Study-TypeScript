daee47eec73bba8c11637554f09d8c85d6677319
강은빈 <kub0803@naver.com>
2021-02-19 18:39:29 +0900

    제네릭

---
+++ TS 연습/dist/제네릭.js
@@ -0,0 +1,10 @@
+"use strict";
+function toArray(a, b) {
+    return [a, b];
+}
+toArray(1, 2);
+function toSilver(a, b) {
+    return [a, b];
+}
+toSilver(2, "silver");
+console.log(toSilver(2, "silver"));
