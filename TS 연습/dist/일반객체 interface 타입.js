c75cd88640490ebd40caa00b4bd6e06f511e1e2f
강은빈 <kub0803@naver.com>
2021-02-21 23:30:26 +0900

    일반객체 interface 함수

--- TS 연습/dist/일반객체 interface 타입.js
+++ TS 연습/dist/일반객체 interface 타입.js
@@ -1,10 +1,23 @@
 "use strict";
-var person = {
-    name: "강은빈",
-    age: 18,
+/* interface Person {
+  name: string;
+  //물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값
+  age?: number;
+}
+interface Developer {
+  name: string;
+  age?: number;
+  skills: string[];
+}
+
+const person: Person = {
+  name: "강은빈",
+  age: 18,
 };
-var expert = {
-    name: "silverbeen",
-    skills: ["react", "JS"],
+
+const expert: Developer = {
+  name: "silverbeen",
+  skills: ["react", "JS"],
 };
-var people = [person, expert];
+
+const people : Person[] = [person, expert]; */ 
