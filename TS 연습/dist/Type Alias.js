bd6d9dabc30ee8ef013082eee89307a10b83da2c
강은빈 <kub0803@naver.com>
2021-02-21 23:30:04 +0900

    Type Alias

---
+++ TS 연습/dist/Type Alias.js
@@ -0,0 +1,11 @@
+"use strict";
+var person = {
+    name: '강은빈'
+};
+var expert = {
+    name: '실버빈',
+    skills: ['react', 'c']
+};
+var people = [person, expert];
+var color = 'red';
+var colors = ['red', 'orange'];
