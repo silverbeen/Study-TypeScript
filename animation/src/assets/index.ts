cf940f47434b39cdb7bb02a2ae8a7b24500f27c8
강은빈 <kub0803@naver.com>
2021-02-26 23:41:03 +0900

    파일 구조 변경

--- animation/src/assets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as earth } from "./earth.png";
-export { default as palne } from "./palne.png";
-export { default as lion } from "./lion.png";
-export { default as 미니언즈 } from "./미니언즈.png";
--- animation/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Rotation } from "./rotation/Rotation";
-export { default as Mouse } from "./mouse/mouse";
-export { default as Dispersion } from "./dispersion/dispersion";
-export { default as SingIn } from "./signIn/SignIn";
-export { default as SignUp } from "./signIn/SignUp";
-export { default as Main } from "./mainPage/Main";
-export { default as Header } from "./mainPage/Header";
