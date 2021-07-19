1c7d20b5ed35c006260cf1c8ead05fa80261d9b6
강은빈 <kub0803@naver.com>
2021-07-20 04:05:29 +0900

    [ADD] pick 기본세팅

---
+++ pick-sub/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import AttendanceDetail from "./components/attendance/attendanceDetail/AttendanceDetail";
+
+function App() {
+  return <AttendanceDetail />;
+}
+
+export default App;
