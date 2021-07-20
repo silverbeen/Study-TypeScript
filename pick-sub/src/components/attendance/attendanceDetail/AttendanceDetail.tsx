d7a74563ff1617c004ea8ba4ba621cd688af73bd
강은빈 <kub0803@naver.com>
2021-07-20 17:46:53 +0900

    [MODIFY] 출석 상세 수정

--- pick-sub/src/components/attendance/attendanceDetail/AttendanceDetail.tsx
+++ pick-sub/src/components/attendance/attendanceDetail/AttendanceDetail.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "../../header/Header";
-import Content from "./Content";
+import SelectionBar from "./SelectionBar";
+import StudentList from "./StudentList";
 import * as S from "./style";
 
 const AttendanceDetail = () => {
   return (
     <S.MainWrapper>
       <Header />
-      <Content />
+      <S.ContentWrapper>
+        <S.Title>
+          <h1 className="club-name">GRAM</h1>
+          <span>세미나실 2-1</span>
+        </S.Title>
+        <SelectionBar />
+        <StudentList />
+      </S.ContentWrapper>
     </S.MainWrapper>
   );
 };
--- pick-sub/src/components/attendance/attendanceDetail/style.ts
+++ pick-sub/src/components/attendance/attendanceDetail/style.ts
@@ -1,4 +1,3 @@
-import { EPERM } from "constants";
 import styled from "styled-components";
 
 export const MainWrapper = styled.div`
@@ -15,15 +14,13 @@ export const MainWrapper = styled.div`
 export const ContentWrapper = styled.div`
   width: 85%;
   height: 85%;
-  border: 1px solid red;
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-size: 19px;
 `;
 
 export const Title = styled.div`
-  border: 1px solid red;
-
   .club-name {
     font-weight: 500;
   }
@@ -31,12 +28,104 @@ export const Title = styled.div`
 
 export const SelectionBar = styled.div`
   width: 100%;
-  height: 4%;
+  height: 3%;
   background-color: white;
+  display: flex;
+  flex-direction: row;
+  margin: 10px;
 `;
 
 export const SelectionItem = styled.div`
+  padding: 3px;
   width: 25%;
   height: 100%;
-  border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// 학생 출석 리스트
+export const StudentWrapper = styled.div`
+  margin-top: 10px;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HeaderBar = styled.ul`
+  width: 90%;
+  height: 5%;
+  border-bottom: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  & li {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    :nth-child(1),
+    :nth-child(2),
+    :nth-child(3) {
+      width: 12.5%;
+    }
+
+    :nth-child(4),
+    :nth-child(5),
+    :nth-child(6) {
+      width: 20%;
+    }
+  }
+`;
+
+export const StudentItem = styled(HeaderBar)`
+  border: none;
+  height: 7%;
+  list-style: none inside;
+
+  & li {
+    :nth-child(1) {
+      & input {
+        width: 100%;
+        height: 25px;
+      }
+    }
+
+    :nth-child(4),
+    :nth-child(5),
+    :nth-child(6) {
+      position: relative;
+      margin: 1%auto;
+      width: 19%;
+      height: 55%;
+      border: 1px solid gray;
+      border-radius: 15px;
+    }
+  }
+
+  .sub-menu {
+    width: 100%;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0 2px 15px #80808061;
+    position: absolute;
+    top: 28px;
+    display: flex;
+    z-index: 1;
+
+    flex-direction: column;
+
+    & li {
+      width: 100%;
+      span {
+        padding: 3px;
+      }
+    }
+  }
 `;
